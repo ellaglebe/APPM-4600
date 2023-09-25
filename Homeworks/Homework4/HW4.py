5b7a87f6bdd714202d0f9a14bb63fd954010809b
ellaglebe <elgl2667@colorado.edu>
2023-09-25 14:32:57 -0600

    hw4 code

---
+++ Homeworks/Homework4/HW4.py
@@ -0,0 +1,21 @@
+import scipy.integrate as integrate
+import numpy as np
+import matplotlib.pyplot as plt
+
+alpha = 0.138 * 10**-6
+t = 3600*60*24
+
+x = np.linspace(0, 2, 100)
+bound = np.array([i/(2*np.sqrt(t*alpha)) for i in x])
+
+tempF = []
+for i in bound:
+    temp, _ = integrate.quad(lambda s: np.exp(-s**2), 0 ,i)
+    tempF.append(temp)
+
+f =  (15/35 - (2/np.sqrt(np.pi))*np.array(tempF))*-1                              
+
+
+plt.plot(x,f)
+plt.show()
+
--- Labs/Lab4/Aitkens.py
+++ Labs/Lab4/Aitkens.py
@@ -74,5 +74,6 @@ def aitkens(vec):
      print(avec)
      print(count1)
      return [avec]
+     
  
 driver()
