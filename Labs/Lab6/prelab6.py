24cd4d9d14d434ce5b9bb26ad9010a16498abda5
ellaglebe <elgl2667@colorado.edu>
2023-10-04 13:05:26 -0600

    prelab

---
+++ Labs/Lab6/prelab6.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+f = lambda x: np.cos(x)
+h = 0.01*2.**(-np.arange(0,10))
+x = np.pi/2
+fp = (f(x+h)-f(x))/h
+print(fp)
+fp1 = (f(x+h)-f(x-h))/2*h
+print(fp1)
+
+
+#Both techniques converge linaerly 
